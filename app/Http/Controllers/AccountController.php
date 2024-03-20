calumshep/event-manager

commit d816981248198bc8e29fcc7667ba01dec404b4b7

Controller audit for redirects and status messages

app/Http/Controllers/AccountController.php
@@ -5,57 +5,46 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UpdateAccountRequest;
 use App\Models\User;
 use Hash;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class AccountController extends Controller
 {
-    public function show(User $user)
+    /**
+     * Show the specified user's details.
+     */
+    public function show(User $user): View
     {
-        return view('accounts.form', [
-            'user' => $user
-        ]);
+        return view('accounts.form', $user);
     }
 
     /**
-     * Show the user's own details
-     *
-     * @return \Illuminate\Contracts\View\View
+     * Show the user's own details.
      */
-    public function showOwn()
+    public function showOwn(): View
     {
-        return view('accounts.form', [
-            'user' => auth()->user()
-        ]);
+        return view('accounts.form', auth()->user());
     }
 
     /**
      * Edit the specified user's details.
-     *
-     * @param \App\Models\User $user
-     *
-     * @return \Illuminate\Contracts\View\View
      */
-    public function edit(User $user)
+    public function edit(User $user): View
     {
-        return view('accounts.form', [
-            'user' => $user
-        ]);
+        return view('accounts.form', $user);
     }
 
     /**
      * Update the user.
-     *
-     * @param \App\Http\Requests\UpdateAccountRequest $request
-     * @param \App\Models\User $user
-     *
-     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(UpdateAccountRequest $request, User $user)
+    public function update(UpdateAccountRequest $request, User $user): RedirectResponse
     {
         $input = $request->validated();
 
         if (! Hash::check($input['current_password'], $user->password)) {
-            return redirect()->back()->withErrors(['Your password was incorrect.']);
+            return redirect()->back()->withErrors([
+                'Your password was incorrect.',
+            ]);
         }
 
         $user->fill([
@@ -72,7 +61,7 @@ class AccountController extends Controller
         $user->save();
 
         return redirect()->route('account.show', $user)->with([
-            'status' => 'Account details updated successfully.'
+            'status' => 'Account details updated successfully.',
         ]);
     }
 }

app/Http/Controllers/HomeController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use Date;
+use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
     /**
      * Display the dashboard.
      */
-    public function dashboard()
+    public function dashboard(): View
     {
         return view('welcome', [
             'events' => Event::where('start', '>=', Date::now())
@@ -21,15 +22,9 @@ class HomeController extends Controller
 
     /**
      * Display the detailed view of an event.
-     *
-     * @param \App\Models\Event $event
-     *
-     * @return \Illuminate\Contracts\View\View
      */
-    public function event(Event $event)
+    public function event(Event $event): View
     {
-        return view('events.detail', [
-            'event'     => $event,
-        ]);
+        return view('events.detail', $event);
     }
 }

app/Http/Controllers/EventController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Str;
 
 class EventController extends Controller
@@ -18,7 +20,7 @@ class EventController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         return view('events.index', [
             'events' => auth()->user()->events->sortBy('start')->paginate(6),
@@ -28,7 +30,7 @@ class EventController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
         return view('events.form', [
             'event'         => new Event(),
@@ -41,7 +43,7 @@ class EventController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreEventRequest $request)
+    public function store(StoreEventRequest $request): RedirectResponse
     {
         $input = $request->validated();
 
@@ -56,14 +58,14 @@ class EventController extends Controller
         ]);
 
         return redirect()->route('events.show', $event)->with([
-            'status' => 'Event successfully created.'
+            'status' => 'Event created successfully.',
         ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Event $event)
+    public function show(Event $event): View
     {
         return view('events.form', [
             'event'         => $event,
@@ -76,7 +78,7 @@ class EventController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Event $event)
+    public function edit(Event $event): View
     {
         return view('events.form', [
             'event'         => $event,
@@ -89,7 +91,7 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateEventRequest $request, Event $event)
+    public function update(UpdateEventRequest $request, Event $event): RedirectResponse
     {
         $input = $request->validated();
 
@@ -105,25 +107,25 @@ class EventController extends Controller
         $event->save();
 
         return redirect()->route('events.show', $event)->with([
-            'status' => 'Event successfully updated.'
+            'status' => 'Event updated successfully.',
         ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Event $event)
+    public function destroy(Event $event): RedirectResponse
     {
         if ($event->tickets->count()) {
             return redirect()->back()->withErrors([
-                "You cannot delete an event which contains tickets. Try deleting the tickets first."
+                'You cannot delete an event which contains tickets. Try deleting the tickets first.',
             ]);
         }
 
         $event->delete();
 
         return redirect()->route('events.index')->with([
-            'warning' => 'Event deleted.'
+            'warning' => 'Event deleted.',
         ]);
     }
 }

app/Http/Controllers/OrderController.php
@@ -6,6 +6,8 @@ use App\Models\Event;
 use App\Models\Guest;
 use App\Models\Order;
 use App\Models\TicketType;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Laravel\Cashier\Cashier;
@@ -22,10 +24,8 @@ class OrderController extends Controller
 
     /**
      * Show all the user's paid orders including tickets.
-     *
-     * @return \Illuminate\Contracts\View\View
      */
-    public function index()
+    public function index(): View
     {
         return view('orders.index', [
             'orders' => auth()->user()->orders
@@ -40,12 +40,8 @@ class OrderController extends Controller
 
     /**
      * Show the specified order.
-     *
-     * @param \App\Models\Order $order
-     *
-     * @return \Illuminate\Contracts\View\View
      */
-    public function show(Order $order)
+    public function show(Order $order): View
     {
         return view('orders.show', [
             'order' => $order->load('tickets'),
@@ -55,10 +51,8 @@ class OrderController extends Controller
 
     /**
      * Show the checkout page for the specified event.
-     *
-     * @return \Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
      */
-    public function checkout(Event $event, Request $request)
+    public function checkout(Event $event, Request $request): RedirectResponse|View
     {
         $tickets = [];
 
@@ -77,7 +71,9 @@ class OrderController extends Controller
                 // Do not proceed if more tickets are requested than available
                 if ($ticket->capacity && $ticket->quantity > $ticket->remaining()) {
                     return redirect()->back()
-                        ->withErrors("You cannot purchase more tickets than are available.");
+                        ->withErrors([
+                            'You cannot purchase more tickets than are available.',
+                    ]);
                 }
 
                 $total += $ticket->price * $ticket->quantity;
@@ -86,7 +82,9 @@ class OrderController extends Controller
         }
 
         if (sizeof($tickets) == 0) {
-            return redirect()->back()->withErrors("You must select at least 1 ticket.");
+            return redirect()->back()->withErrors([
+                'You must select at least 1 ticket.',
+            ]);
         }
 
         return view('tickets.checkout', [
@@ -109,14 +107,8 @@ class OrderController extends Controller
      *      "name_2" => array:1 [▼
      *          0 => "test 3"
      *      ]
-     *
-     * @param \App\Models\Event $event
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Laravel\Cashier\Checkout
-     * @throws \Exception
      */
-    public function purchase(Event $event, Request $request)
+    public function purchase(Event $event, Request $request): RedirectResponse|Checkout
     {
         /*
          * Process a new order
@@ -191,9 +183,9 @@ class OrderController extends Controller
             'cancel_url'    =>      // redirect if checkout actively cancelled by user
                 route('event.tickets.cancelled', $event),
             'metadata'      => [    // give Stripe the order ID so it can pass this back in the webhook after payment
-                'order_id' => $order->id],
-            'expires_at' =>         // set checkout session expiry to 1 hour from now
-                now()->addHour()->timestamp
+                'order_id'  => $order->id],
+            'expires_at'    =>      // set checkout session expiry to 1 hour from now
+                now()->addHour()->timestamp,
         ];
 
         if (auth()->user()) {       // start a checkout session on the authenticated user
@@ -206,27 +198,18 @@ class OrderController extends Controller
 
     /**
      * Handle Stripe redirecting back form a cancelled Checkout session.
-     *
-     * @param \App\Models\Event $event
-     *
-     * @return \Illuminate\Http\RedirectResponse
      */
-    public function cancelled(Event $event)
+    public function cancelled(Event $event): RedirectResponse
     {
         return redirect()->route('home.event', $event)->with([
-            'warning' => "Your ticket purchase was cancelled.",
+            'warning' => 'Your ticket purchase was cancelled.',
         ]);
     }
 
     /**
      * Handle successful completion of a Stripe Checkout session.
-     *
-     * @param \App\Models\Event $event
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
      */
-    public function success(Event $event, Request $request)
+    public function success(Event $event, Request $request): View|RedirectResponse
     {
         try {
             return view('tickets.success', [
@@ -237,7 +220,7 @@ class OrderController extends Controller
             Log::error($e);
 
             return redirect()->route('home.event', $event)->withErrors([
-                "An unknown error occurred."
+                'An unknown error occurred.'
             ]);
         }
     }

app/Http/Controllers/OrganisationController.php
@@ -49,8 +49,8 @@ class OrganisationController extends Controller
             'website'       => $input['website'],
         ]);
 
-        return redirect()->route('organisations.index', [
-            'organisation' => $organisation,
+        return redirect()->route('organisations.show', $organisation)->with([
+            'status' => 'Organisation created successfully.',
         ]);
     }
 
@@ -92,8 +92,8 @@ class OrganisationController extends Controller
         ]);
         $organisation->save();
 
-        return redirect()->route('organisations.show', [
-            'organisation' => $organisation,
+        return redirect()->route('organisations.show', $organisation)->with([
+            'status' => 'Organisation updated successfully.',
         ]);
     }
 
@@ -104,14 +104,14 @@ class OrganisationController extends Controller
     {
         if ($organisation->events->count()) {
             return redirect()->back()->withErrors([
-                "You cannot delete an organisation which has events associated with it. Try deleting the events first."
+                'You cannot delete an organisation which has events associated with it. Try deleting the events first.',
             ]);
         }
 
         $organisation->delete();
 
         return redirect()->route('organisations.index')->with([
-            'warning' => 'Organisation deleted.'
+            'warning' => 'Organisation deleted successfully.',
         ]);
     }
 }

app/Http/Controllers/TicketTypeController.php
@@ -6,7 +6,9 @@ use App\Http\Requests\StoreTicketRequest;
 use App\Http\Requests\UpdateTicketRequest;
 use App\Models\Event;
 use App\Models\TicketType;
+use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\RedirectResponse;
 
 class TicketTypeController extends Controller
 {
@@ -19,7 +21,7 @@ class TicketTypeController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(Event $event)
+    public function create(Event $event): View
     {
         return view('tickets.form', [
             'ticket'        => new TicketType(),
@@ -34,7 +36,7 @@ class TicketTypeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Event $event, StoreTicketRequest $request)
+    public function store(Event $event, StoreTicketRequest $request): RedirectResponse
     {
         $input = $request->validated();
 
@@ -49,14 +51,14 @@ class TicketTypeController extends Controller
         ]);
 
         return redirect()->route('events.tickets.show', [$event, $ticket_type])->with([
-            'status' => 'Ticket successfully created.'
+            'status' => 'Ticket created successfully.'
         ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Event $event, TicketType $ticket_type)
+    public function show(Event $event, TicketType $ticket_type): View
     {
         return view('tickets.form', [
             'ticket'        => $ticket_type,
@@ -71,7 +73,7 @@ class TicketTypeController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Event $event, TicketType $ticket_type)
+    public function edit(Event $event, TicketType $ticket_type): View
     {
         return view('tickets.form', [
             'ticket'        => $ticket_type,
@@ -86,7 +88,7 @@ class TicketTypeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTicketRequest $request, Event $event, TicketType $ticket_type)
+    public function update(UpdateTicketRequest $request, Event $event, TicketType $ticket_type): RedirectResponse
     {
         $input = $request->validated();
 
@@ -102,34 +104,32 @@ class TicketTypeController extends Controller
         $ticket_type->save();
 
         return redirect()->route('events.tickets.show', [$event, $ticket_type])->with([
-            'status' => 'Ticket details updated successfully.'
+            'status' => 'Ticket updated successfully.'
         ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Event $event, TicketType $ticket_type)
+    public function destroy(Event $event, TicketType $ticket_type): RedirectResponse
     {
         if ($ticket_type->orders->count()) {
             return redirect()->back()->withErrors([
-                "You cannot delete a ticket which has orders."
+                'You cannot delete a ticket which has orders.'
             ]);
         }
 
         $ticket_type->delete();
 
         return redirect()->route('events.show', $event)->with([
-            'warning' => "Ticket successfully deleted.",
+            'warning' => 'Ticket deleted.',
         ]);
     }
 
     /**
      * Compute details for information collection on ticket.
-     *
-     * @return array
      */
-    public function computeDetails(FormRequest $request)
+    public function computeDetails(FormRequest $request): array
     {
         $details = [];
         if ($request->dob) {

commit d7d954cd94b4786fbd139dd7f4cb9ee3ba5aa161

Fix breaking changes introduced in d816981248198bc8e29fcc7667ba01dec404b4b7

app/Http/Controllers/AccountController.php
@@ -15,7 +15,9 @@ class AccountController extends Controller
      */
     public function show(User $user): View
     {
-        return view('accounts.form', $user);
+        return view('accounts.form', [
+            'user' => $user,
+        ]);
     }
 
     /**
@@ -23,7 +25,9 @@ class AccountController extends Controller
      */
     public function showOwn(): View
     {
-        return view('accounts.form', auth()->user());
+        return view('accounts.form', [
+            'user' => auth()->user()
+        ]);
     }
 
     /**
@@ -31,7 +35,9 @@ class AccountController extends Controller
      */
     public function edit(User $user): View
     {
-        return view('accounts.form', $user);
+        return view('accounts.form', [
+            'user' => $user
+        ]);
     }
 
     /**

app/Http/Controllers/EventSalesController.php
@@ -5,17 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Event;
 use App\Models\Order;
 use App\Models\OrderTicket;
+use Illuminate\Contracts\View\View;
 
 class EventSalesController extends Controller
 {
     /**
      * Show the event sales view.
-     *
-     * @param \App\Models\Event $event
-     *
-     * @return \Illuminate\Contracts\View\View
      */
-    public function sales(Event $event)
+    public function sales(Event $event): View
     {
         return view('events.sales', [
             'event'             => $event,
@@ -30,12 +27,8 @@ class EventSalesController extends Controller
 
     /**
      * Export event sales to a CSV file and download to the browser.
-     *
-     * @param \App\Models\Event $event
-     *
-     * @return void
      */
-    public function exportSales(Event $event)
+    public function exportSales(Event $event): void
     {
         $this->prepareFile(
             filename: 'event_' . $event->id . '_sales_' . now()->toDateTimeLocalString() . '.csv',
@@ -59,12 +52,8 @@ class EventSalesController extends Controller
 
     /**
      * Show the attendee list view.
-     *
-     * @param \App\Models\Event $event
-     *
-     * @return \Illuminate\Contracts\View\View
      */
-    public function attendees(Event $event)
+    public function attendees(Event $event): View
     {
         return view('events.attendees', [
             'event'     => $event,
@@ -72,7 +61,10 @@ class EventSalesController extends Controller
         ]);
     }
 
-    public function exportAttendees(Event $event)
+    /**
+     * Export attendee list to a CSV file and download to the browser.
+     */
+    public function exportAttendees(Event $event): void
     {
 
         $this->prepareFile(filename: 'event_' . $event->id . '_attendees_' . now()->toDateTimeLocalString() . '.csv',
@@ -104,7 +96,14 @@ class EventSalesController extends Controller
         );
     }
 
-    public function prepareFile(string $filename, array $headers, array $data)
+    /**
+     * Prepare a file stream for output.
+     *
+     * @param string $filename The name of the file to be downloaded, including extension (.csv).
+     * @param array $headers Array of column headers to be used for the header row. Must be in the same order as data.
+     * @param array $data 2D array of data (row-by-row).
+     */
+    public function prepareFile(string $filename, array $headers, array $data): void
     {
         ob_start();
 

app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     public function event(Event $event): View
     {
         return view('events.detail', [
-            'event'     => $event,
+            'event' => $event,
         ]);
     }
 }