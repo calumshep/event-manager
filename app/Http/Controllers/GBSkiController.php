7d8a10fa482fa503051b3c2937cd0d0ca9680950
Calum Shepherd <calum.shep@icloud.com>
2024-05-22 14:16:05 +0100

    Implement daily update of (and API endpoint to retrieve) GBSki data

--- app/Http/Controllers/GBSkiController.php
+++ app/Http/Controllers/GBSkiController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class GBSkiController extends Controller
 {
     public function activeRegistrations()
     {
-        $response = Http::get('https://www.gbski.com/competitors.php?csv&season=2024');
+        // Parse CSV into competitor objects (from https://www.php.net/manual/en/function.str-getcsv.php)
+        $data = str_getcsv(Storage::get('competitors.csv'), "\n");
+        foreach($data as &$row) {
+            $row = str_getcsv($row); // parse the items in rows
+        }
 
-        return $response;
+        array_walk($data, function(&$a) use ($data) {
+            $a = array_combine($data[0], $a); // object-ify data
+        });
+        array_shift($data); // remove column header
+
+        return response()->json($data);
     }
 }
