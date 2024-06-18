4be0487eeed658bdeb7e14af2ba2f0b646c854b9
Calum Shepherd <calum.shep@icloud.com>
2024-06-18 15:56:07 +0100

    Fix pagination to show 0 of 0 results for an empty collection

--- resources/views/vendor/pagination/bootstrap-5.blade.php
+++ resources/views/vendor/pagination/bootstrap-5.blade.php
@@ -1,6 +1,12 @@
-<div>
-    Showing {{ ($paginator->firstItem() )}} to {{ $paginator->lastItem() }} of {{ $paginator->total() }} entries
-</div>
+@if($paginator->total() > 0)
+    <div>
+        Showing {{ ($paginator->firstItem() )}} to {{ $paginator->lastItem() }} of {{ $paginator->total() }} entries
+    </div>
+@else
+    <div>
+        Showing 0 of 0 entries
+    </div>
+@endif
 
 @if ($paginator->hasPages())
     <nav class="d-flex justify-items-center justify-content-between">
